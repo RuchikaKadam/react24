261917a68fdc965b68684b1b2224a238576acc8d
RuchikaKadam <kadamruchikafullstack.com>
2024-06-24 09:11:14 +0530

    cra

---
+++ src/config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDQ-lfQV9z4HVPqoYeErv1zKKjDMbqa9b0",
+    authDomain: "fir-demom5.firebaseapp.com",
+    projectId: "fir-demom5",
+    storageBucket: "fir-demom5.appspot.com",
+    messagingSenderId: "494614335207",
+    appId: "1:494614335207:web:0d40d499013932600adc42"
+  };
